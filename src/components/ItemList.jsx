255f33a382f5b8a9307bbba98417013519698033
Franco Novack <franconovack5@gmail.com>
2024-11-23 20:33:21 -0300

    pre-entrega

---
+++ src/components/ItemList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Item from './Item'
+
+const ItemList = ({products}) => {
+return (
+    <div className='d-flex justify-content-around aling-items-center flex-wrap'>
+        {products.map((product)=> <Item product={product} key={product.id} />)}
+    </div>
+    
+  )
+}
+
+export default ItemList
